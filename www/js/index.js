3cb1619f145cef028c676f0833edd4f1c8066b06
Mehul Gawde <mehulgawdenxt@gmail.com>
2017-09-30 20:34:31 +0530

    Cleaned up the project. Used contacts plugin and Camera plugin.

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,23 @@
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
-        
+        document.getElementById("cameraTakePicture").addEventListener
+            ("click", cameraTakePicture);
+        document.getElementById("addContact").addEventListener
+            ("click", createContact);
+        document.getElementById("findContact").addEventListener
+            ("click", findContacts);
+        document.getElementById("deleteContact").addEventListener
+            ("click", deleteContact);
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -23,3 +30,72 @@ var app = {
 };
 
 app.initialize();
+
+function cameraTakePicture() {
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
+
+    function onSuccess(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
+function createContact() {
+    // create a new contact object
+    var contact = navigator.contacts.create({ "displayName": "Jon Doe" });
+    // save to device
+    contact.save(onSuccess, onError);
+    function onSuccess(contact) {
+        alert("Save Success");
+    };
+
+    function onError(contactError) {
+        alert("Error = " + contactError.code);
+    };
+}
+function findContacts() {
+    var options = new ContactFindOptions();
+    options.filter = "Jon Doe";
+    options.multiple = false;
+    fields = ["displayName"];
+    navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+
+    function contactfindSuccess(contacts) {
+        for (var i = 0; i < contacts.length; i++) {
+            alert("Display Name = " + contacts[i].displayName);
+        }
+    }
+
+    function contactfindError(message) {
+        alert('Failed because: ' + message);
+    }
+}
+function deleteContact() {
+    var options = new ContactFindOptions();
+    options.filter = "Jon Doe";
+    options.multiple = false;
+    fields = ["displayName"];
+    navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+
+    function contactfindSuccess(contacts) {
+        var contact = contacts[0]
+        contact.remove(onSuccess, onError);
+        function onSuccess() {
+            alert("Removal Success");
+        };
+        
+        function onError(contactError) {
+            alert("Error = " + contactError.code);
+        };
+    }
+
+    function contactfindError(message) {
+        alert('Failed because: ' + message);
+    }
+}
